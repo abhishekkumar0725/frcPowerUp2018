a0a3b1390352fd74888332e742f53d1a5451f92a
abhishekkumar0725 <39935972+abhishekkumar0725@users.noreply.github.com>
2019-01-11 14:18:24 -0500

    Delete bye.java

--- src/org/usfirst/frc/team2872/robot/bye.java
+++
@@ -1,7 +0,0 @@
-package org.usfirst.frc.team2872.robot;
-
-public class bye {
-	public static void main(String[] args) {
-		System.out.println("\"Who told you my catch phrase\"\n\t\t      -Steven Popovitz, 2018");
-		}
-}	
